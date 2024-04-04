b3fb013c031f6d49f1fd5f4ef622a81efc144127
Alejandro Reyes <reyesbalejandroe@gmail.com>
2024-04-04 17:26:24 +0200

    add/solution

--- solution.js
+++ solution.js
@@ -1,15 +1,11 @@
 for (let number = 1; number <= 1000; number++) {
-
-    if (number % 3 === 0 && number % 5 === 0) {
-        console.log("FizzBuzz");
-    }
-    else if (number % 5 === 0) {
-        console.log("Buzz");
-    } 
-    else if (number % 3 === 0) {
-        console.log("Fizz");
-    } 
-    else {
-        console.log(number);
-    }
+  if (number % 3 === 0 && number % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (number % 5 === 0) {
+    console.log("Buzz");
+  } else if (number % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(number);
+  }
 }
